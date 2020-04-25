32bdb139008a8810a5140ad596ea3793b0319992
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-04-25 16:29:08 +0700

    Add %.2f for display printf

--- FirstProject/Source.c
+++ FirstProject/Source.c
@@ -2,16 +2,7 @@
 #include<stdbool.h>
 int main()
 {
-    int integerVar = 100;
-    float floatingVar = 331.79;
-    double doubleVar = 8.44e+11;
-    char charVar = 'W';
-    _Bool boolVar = 0;
-    printf("integerVar = %i\n", integerVar);
-    printf("floatVar = %f\n", floatingVar);
-    printf("doubleVar = %e\n", doubleVar);
-    printf("doubleVar = %g\n", doubleVar);
-    printf("charVar = %c\n", charVar);
-    printf("boolVar= %i\n", boolVar);
+    float x = 3.999993939;
+    printf("%.5f", x);
     return 0;
 }
