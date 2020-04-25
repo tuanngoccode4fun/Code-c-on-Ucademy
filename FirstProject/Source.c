92791e94ae8ca2efe7d0670f259a47585b672c5c
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-04-25 15:12:58 +0700

    #include <stdbool.h>

--- FirstProject/Source.c
+++ FirstProject/Source.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<stdbool.h>
 int main()
 {
-    float jason = 23.333;
+  /*  float jason = 23.333;
     double jason2 = 55.5555555555555e+11;
-    _Bool boolVariable = 1;
+    _Bool boolVariable = 1;*/
+    bool trueBoolean = true;
+    bool falseBoolean = false;
 
     return 0;
 }
