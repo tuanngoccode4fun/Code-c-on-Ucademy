07a521d31bfb625655d0d9766d919874bfb5eda4
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-04-25 16:17:29 +0700

    Show integer,double,float,char,_Bool by printf

--- FirstProject/Source.c
+++ FirstProject/Source.c
@@ -2,7 +2,16 @@
 #include<stdbool.h>
 int main()
 {
-    int sum = 98;
-    printf("The sum is %d\n", sum);
+    int integerVar = 100;
+    float floatingVar = 331.79;
+    double doubleVar = 8.44e+11;
+    char charVar = 'W';
+    _Bool boolVar = 0;
+    printf("integerVar = %i\n", integerVar);
+    printf("floatVar = %f\n", floatingVar);
+    printf("doubleVar = %e\n", doubleVar);
+    printf("doubleVar = %g\n", doubleVar);
+    printf("charVar = %c\n", charVar);
+    printf("boolVar= %i\n", boolVar);
     return 0;
 }
